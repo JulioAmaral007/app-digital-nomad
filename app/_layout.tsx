6c0a573cc10ee7b372114639db66888dee8d55fd
Júlio Cézar <jczrpereira77@gmail.com>
2025-11-02 20:09:05 -0300

    feat(layout): add city screen to RootLayout and update CityItem navigation to link to city details

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,10 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack
-      screenOptions={{ headerShown: false, fullScreenGestureEnabled: true }}
-    >
+      screenOptions={{ headerShown: false, fullScreenGestureEnabled: true }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="about" />
+      <Stack.Screen name="city/[id]" />
     </Stack>
   );
 }
