ef7abb66ddfc35ca2b6208055dc0f438aebd0746
Júlio Cézar <jczrpereira77@gmail.com>
2025-11-02 19:59:42 -0300

    feat(city-list): implement city list display using FlatList and CityItem component

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { CityItem } from "@/components/CityItem";
+import { cities } from "@/data/cities";
+import { FlatList, StyleSheet, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={cities}
+        renderItem={({ item }) => <CityItem city={item} />}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#1B1B1B",
+    flex: 1,
+  },
+});
