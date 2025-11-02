7e9e342d386872d471206a8d6e3bffb646ecbe9f
Júlio Cézar <jczrpereira77@gmail.com>
2025-11-02 20:03:55 -0300

    feat(city-item): add cover image and improve layout in CityItem component; enhance Index component with horizontal padding and item separators

--- app/index.tsx
+++ app/index.tsx
@@ -5,13 +5,27 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
   const { top, bottom } = useSafeAreaInsets();
-  
-  return (
+
+return (
     <View style={styles.container}>
       <FlatList
-        contentContainerStyle={{ paddingTop: top, paddingBottom: bottom }}
+        contentContainerStyle={{
+          paddingTop: top,
+          paddingBottom: bottom,
+          paddingHorizontal: 16,
+        }}
         data={cities}
         renderItem={({ item }) => <CityItem city={item} />}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{
+              height: 1,
+              backgroundColor: "#5c5c5c",
+              marginVertical: 32,
+            }}
+          />
+        )}
       />
     </View>
   );
